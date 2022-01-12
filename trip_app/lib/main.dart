2cc49951a417734874a72fba0ce8c498ad924bdd
raniakbr <kbrrania@gmail.com>
2022-01-12 22:59:20 +0100

    Trip app tutorial - part 2

--- trip_app/lib/main.dart
+++ trip_app/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:trip_app_/fade_animation.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Nunitoo'),
       home: HomePage(),
     ));
 
@@ -12,8 +14,10 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   PageController? _pageController;
+  int totalPage = 4;
 
   void _onScroll() {}
 
@@ -32,18 +36,22 @@ class _HomePageState extends State<HomePage> {
       controller: _pageController,
       children: <Widget>[
         makePage(
+            page: 1,
             image: 'assets/images/four.jpg',
             title: 'Title 01',
             description: 'description 01'),
         makePage(
+            page: 2,
             image: 'assets/images/three.jpg',
             title: 'Title 02',
             description: 'description 02'),
         makePage(
+            page: 3,
             image: 'assets/images/two.jpg',
             title: 'Title 03',
             description: 'description 03'),
         makePage(
+            page: 4,
             image: 'assets/images/one.jpg',
             title: 'Title 04',
             description: 'description 04'),
@@ -51,7 +59,7 @@ class _HomePageState extends State<HomePage> {
     ));
   }
 
-  Widget makePage({image, title, description}) {
+  Widget makePage({page, image, title, description}) {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
@@ -81,14 +89,14 @@ class _HomePageState extends State<HomePage> {
                     height: 40,
                   ),
                   Text(
-                    '1',
+                    page.toString(),
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 30,
                         fontWeight: FontWeight.bold),
                   ),
                   Text(
-                    '/4',
+                    '/' + totalPage.toString(),
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 15,
@@ -100,81 +108,100 @@ class _HomePageState extends State<HomePage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Text(
-                    title,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold),
-                  ),
+                  FadeAnimation(
+                      1,
+                      Text(
+                        title,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold),
+                      )),
                   SizedBox(
                     height: 20,
                   ),
-                  Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(right: 5),
-                        child: Icon(
-                          Icons.star,
-                          color: Colors.yellow,
-                          size: 15,
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(right: 5),
-                        child: Icon(
-                          Icons.star,
-                          color: Colors.yellow,
-                          size: 15,
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(right: 5),
-                        child: Icon(
-                          Icons.star,
-                          color: Colors.yellow,
-                          size: 15,
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(right: 5),
-                        child: Icon(
-                          Icons.star,
-                          color: Colors.yellow,
-                          size: 15,
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(right: 5),
-                        child: Icon(
-                          Icons.star,
-                          color: Colors.grey,
-                          size: 15,
-                        ),
-                      ),
-                      Text(
-                        '4.0',
-                        style: TextStyle(color: Colors.white70),
-                      ),
-                      Text(
-                        '(2300)',
-                        style: TextStyle(color: Colors.white38, fontSize: 12),
-                      )
-                    ],
-                  ),
+                  FadeAnimation(
+                      1.5,
+                      Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(right: 5),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 5),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 5),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 5),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 5),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.grey,
+                              size: 15,
+                            ),
+                          ),
+                          Text(
+                            '4.0',
+                            style: TextStyle(color: Colors.white70),
+                          ),
+                          Text(
+                            '(2300)',
+                            style:
+                                TextStyle(color: Colors.white38, fontSize: 12),
+                          )
+                        ],
+                      )),
                   SizedBox(
                     height: 20,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 50),
-                    child: Text(
-                      description,
-                      style: TextStyle(
-                          color: Colors.white.withOpacity(.7),
-                          height: 1.5,
-                          fontSize: 15),
-                    ),
-                  )
+                  FadeAnimation(
+                      2,
+                      Padding(
+                        padding: const EdgeInsets.only(right: 50),
+                        child: Text(
+                          description,
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(.7),
+                              height: 1.5,
+                              fontSize: 15),
+                        ),
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  FadeAnimation(
+                      2.5,
+                      Text(
+                        'READ MORE',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
                 ],
               ),
             )
